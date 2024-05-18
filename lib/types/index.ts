4daa6794f2d24ee99a94d173dbd5caf2fa555a36
sh8ee <121178695+sh3ee@users.noreply.github.com>
2024-05-18 11:06:39 +0530

    `.ts Variables` more optimize to display Variables smoothly

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,60 +1,63 @@
-import { PermissionsString } from 'discord.js';
+import { PermissionsString, RESTPostAPIApplicationCommandsJSONBody } from 'discord.js';
 
-
-interface CommandPermissionOptions {
-  botPermissions: PermissionsString[]; userPermissions: PermissionsString[];
+interface AdditionalOptions {
+  /**
+   * A string or array of permissions that the bot needs to execute the current command.
+   *
+   * @example
+   * botPermissions: 'KickMembers'
+   * or
+   * botPermissions: ['KickMembers', 'ModerateMembers']
+   */
+  botPermissions?: PermissionsString | PermissionsString[];
+  /**
+   * A string or array of permissions that a user needs for the current command to be executed.
+   *
+   * @example
+   * userPermissions: 'KickMembers'
+   * or
+   * userPermissions: ['KickMembers', 'ModerateMembers']
+   */
+  userPermissions?: PermissionsString | PermissionsString[];
+  /**
+   * A boolean value that indicates whether the command is for developer-only registration or not.
+   */
   devOnly?: boolean;
 };
 
-interface SubAndGroupCommandOptions {
-  type: number; /* ApplicationCommandOptionType.<> */
-  required?: boolean; // required (default): false
-  name: string;
-  description: string;
-  channelTypes?: number[]; /* [ChannelType.<>] */
-  autocomplete?: boolean;
-  choices?: { name: string, value: string }[];
-  maxValue?: number;
-  minValue?: number;
-  minLength?: number;
-  maxLength?: number;
-};
 
-interface CommandOptions {
-  type: number; /* ApplicationCommandOptionType.<> */
-  required?: boolean; // required (default): false
-  name: string;
-  description: string;
-  options: SubAndGroupCommandOptions[];
-  channelTypes?: number[]; /* [ChannelType.<>] */
-  autocomplete?: boolean;
-  choices?: { name: string, value: string }[];
-  maxValue?: number;
-  minValue?: number;
-  minLength?: number;
-  maxLength?: number;
-};
-
-interface SlashCommandsData {
-  type?: number; /* ApplicationCommandType.<> */
-  name: string;
-  description: string;
-  nsfw?: boolean;
-  options?: CommandOptions[];
-  dmPermission?: boolean;
-  defaultMemberPermissions?: PermissionsString[];
-  perms: CommandPermissionOptions;
+export interface SlashCommandsData {
+  data: RESTPostAPIApplicationCommandsJSONBody,
+  others: AdditionalOptions;
   callback: (options: { client: import('../../structures/classes/customclient.js').CustomClient, interaction: import('discord.js').CommandInteraction }) => Promise<any>;
 };
 
-interface MessageCommandsData {
+export interface MessageCommandsData {
+  /**
+   * The names that the bot needs to execute the current MessageCommands.
+   */
   name: string;
+  /**
+   * The `aliases` are an array of multiple MessageCommands names set that the bot uses to execute the current command.
+   * 
+   * @example
+   *  aliases: ['ping','ms','ws']
+   */
   aliases?: string[];
+  /**
+   * The name of the `MessageCommands` should ideally be between `1 to 100` characters for optimal readability and efficiency, `only for your understanding`. 
+   * However, `you can extend this limit` based on your specific requirements.
+   */
   description?: string;
-  perms: CommandPermissionOptions;
+  /**
+   * The `example` of how to use MessageCommands shows that you can add multiple commands in a string or an array.
+   *  
+   * @example
+   * example: ['!ban <user> <reason>','!ban <user>']
+   */
+  example?: string[];
+  others: AdditionalOptions;
   callback: (options: { client: import('../../structures/classes/customclient.js').CustomClient, message: import('discord.js').Message, args: string[] }) => Promise<any>;
 };
 
-
-export type SLASH = SlashCommandsData;
-export type MESSAGE = MessageCommandsData;
+export { };
