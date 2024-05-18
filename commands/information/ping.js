50a793f220c0fd6bc6723ccc7092781dd2fa959b
sh8ee <121178695+sh3ee@users.noreply.github.com>
2024-05-18 11:04:37 +0530

    `callback` changed to `script`. and `.ts` Variables `MESSAGE` become to a `MessageCommandsData`

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,17 +1,18 @@
 const { Message } = require('discord.js');
-/** @type {import('../../lib/types/index.ts').MESSAGE} */
+/** @type {import('../../lib/types/index.ts').MessageCommandsData} */
 
 module.exports = {
   name: 'ping',
   aliases: ['ms', 'ws'],
   description: 'Get info about bot',
-
-  perms: {
+  example: [],
+   
+  others: {
     botPermissions: ['SendMessages'], userPermissions: ['SendMessages'],
     devOnly: true,
   },
 
-  callback: async ({ client, message, args }) => {
+  script: async ({ client, message, args }) => {
     return message.reply({ content: `Ping: **${client.ws.ping} ms**` });
   }
 }
