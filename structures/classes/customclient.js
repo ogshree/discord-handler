8e243bc176e1d8cd93d96ae58a6295bcabe70d24
sh8ee <121178695+sh3ee@users.noreply.github.com>
2024-05-19 10:47:38 +0530

    format document

--- structures/classes/customclient.js
+++ structures/classes/customclient.js
@@ -1,9 +1,16 @@
-const { developerIds, clientToken } = require('../../lib/configuration.json');
-const { Client, Collection } = require('discord.js');
+const {
+  developerIds,
+  clientToken
+} = require('../../lib/configuration.json');
+const {
+  Client,
+  Collection
+} = require('discord.js');
 
 class CustomClient extends Client {
   MessageCommands = new Collection();
   SlashCommands = new Collection();
+
   Icon = require('../../lib/plugins/design/icons.js').Icon;
   Developer = developerIds;
 
