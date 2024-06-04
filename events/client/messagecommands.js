71eba54d95278281dc1044f2b49ffd6e0285864b
sh8ee <121178695+sh3ee@users.noreply.github.com>
2024-06-04 21:16:26 +0530

    Remove only `GuildText` 'CHANNEL' priority

--- events/client/messagecommands.js
+++ events/client/messagecommands.js
@@ -1,4 +1,4 @@
-const { Events, Message, ChannelType, EmbedBuilder } = require('discord.js');
+const { Events, Message, EmbedBuilder } = require('discord.js');
 const { CustomClient } = require('../../structures/classes/customclient.js');
 const { clientPrefix } = require('../../lib/configuration.json');
 
@@ -12,28 +12,22 @@ module.exports = {
 
     script: async (client, message) => {
 
-        if (message.channel.type !== ChannelType.GuildText) return;
-
         const { author, guild, member } = message;
-        const prefix = clientPrefix;
+        if (author.bot || !message.guild || !message.content.toLowerCase().startsWith(clientPrefix)) return;
 
-        if (author.bot || !message.guild || !message.content.toLowerCase().startsWith(prefix)) return;
-
-        const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+        const [cmd, ...args] = message.content.slice(clientPrefix.length).trim().split(/ +/g);
         const command = client.MessageCommands.get(cmd.toLowerCase()) || client.MessageCommands.find(c => c.aliases?.includes(cmd.toLowerCase()));
 
         if (!command) return;
         const embed = new EmbedBuilder().setColor('Red')
 
+        if (command.others.devOnly && !client.Developer.includes(author.id)) return;
+        
         if (command.others.userPermissions && command.others.userPermissions.length !== 0)
             if (!member.permissions.has(command.others.userPermissions)) return message.reply({ embeds: [embed.setDescription(`${client.Icon.Static.Wrong} You need \`${command.others.userPermissions || command.others.userPermissions.join(", ")}\` permission(s) to execute this command!`)] });
 
-
         if (command.others.botPermissions && command.others.botPermissions.length !== 0)
             if (!guild.members.me.permissions.has(command.others.botPermissions)) return message.reply({ embeds: [embed.setDescription(`${client.Icon.Static.Wrong} I need \`${command.others.botPermissions || command.others.botPermissions.join(", ")}\` permission(s) to execute this command!`)] });
-
-
-        if (command.others.devOnly && !client.Developer.includes(author.id)) return;
 
         command.script({ client, message, args });
     }
